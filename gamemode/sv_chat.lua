81db24c32441b3a4bcd59e0dd538b3dc96eeb8a6
c1yd3i <breslowrocky@gmail.com>
2015-01-01 16:28:29 -0500

    Updates

--- gamemode/sv_chat.lua
+++ gamemode/sv_chat.lua
@@ -1,4 +1,5 @@
 function GM:PlayerCanSeePlayersChat( text, teamOnly, listener, speaker )
+  if !isRoundActive then return true; end
   if !listener:IsAlive() then return true; end
   if teamOnly && speaker:IsAlive() then return true; end
   if !speaker:IsAlive() && !listener:IsAlive() then return true; end
