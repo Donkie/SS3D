0d657d166efcafe6961a88a4da32fcdfa79d6735
c1yd3i <breslowrocky@gmail.com>
2015-01-01 15:52:49 -0500

    freshman mistake

--- gamemode/sv_chat.lua
+++ gamemode/sv_chat.lua
@@ -1,7 +1,7 @@
 function GM:PlayerCanSeePlayersChat( text, teamOnly, listener, speaker )
   if !listener:IsAlive() then return true; end
   if teamOnly && speaker:IsAlive() then return true; end
-  if !speaker:IsAlive() && !listener:IsAlive() then return true;
+  if !speaker:IsAlive() && !listener:IsAlive() then return true; end
 
   return false;
 end
